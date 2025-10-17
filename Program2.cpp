7157b42909678a2ae16742d6de81c1c1868a4c8c
GOHIT11 <gohitdevarakonda2007@gmail.com>
2025-10-17 09:54:46 +0530

    Update Program2.cpp

--- Program2.cpp
+++ Program2.cpp
@@ -1,4 +1,5 @@
 // To write a c++ program that demonstrates concept of call by value and call by reference
+// Call by value 
 #include <iostream>
 using namespace std;
 // Function to modify value
@@ -10,5 +11,22 @@ int main() {
  cout << "Value of a before function call: " << a << endl ;
  modify(a) ; // Call by Value
  cout << "Value of a after function call: " << a << endl;
+ return 0;
+}
+//Call by Reference 
+#include <iostream>
+using namespace std;
+void swapNumbers(int &a, int &b) {
+ int temp = a;
+ a = b;
+ b = temp;
+ cout << "Inside function (after swap): a = " << a << ", b = " << b << endl;
+}
+int main() {
+ int x = 10, y = 20;
+ cout << "Before function call: x = " << x << ", y = " << y << endl;
+ // Call by reference
+ swapNumbers(x, y);
+ cout << "After function call: x = " << x << ", y = " << y << endl;
  return 0;
 }
